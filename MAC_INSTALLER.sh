b7f25a2149043529bcd30a3182f248d7d6baa5e5
Wil Sprouse <wilsprouse@fastmail.com>
2023-07-01 14:45:24 -0400

    More Updates

--- MAC_INSTALLER.sh
+++ MAC_INSTALLER.sh
@@ -1,12 +1,12 @@
 # Check for Python 3
-if [[ "$(python3 -V)" =~ "Python 3" ]]
+if [[ "$(python -V)" =~ "Python 3" ]]
 then
     echo "Python 3 is installed"
     echo "Continuing with download..."
 else
     echo "We recommend Python 3 to be used, but it is not installed"
     read -p "Continue install without Python 3 (Y/N)? " install
-    if [["$install" == "Y" || "$install" == "y"]]
+    if [[ "$install" == "Y" || "$install" == "y" ]]
     then
         echo "Upgrading to latest Python Version"
         brew upgrade python3
